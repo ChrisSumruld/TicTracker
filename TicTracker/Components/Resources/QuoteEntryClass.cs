b5f1d5b0f7d7ef0eabd302be9a87ecc6d05f973e
ChrisSumruld <aperix123@gmail.com>
2026-09-15 12:23:58 -0500

    Buttons implemented and QuoteEntry class initializaed, need to create database to store QuoteEntry objects in next push.

---
+++ TicTracker/Components/Resources/QuoteEntryClass.cs
@@ -0,0 +1,24 @@
+﻿namespace TicTracker.Components.Resources
+{
+    public class QuoteEntry
+    {
+        //public 
+        public QuoteEntry(int entryType)
+        {
+            this.entryType = entryType;
+            this.entryTime = DateTime.Now;
+        }
+
+        int entryType;
+        DateTime entryTime;
+        private readonly Dictionary<int, string> entryToQuoteTranslation = new()
+        {
+            {1, "It's Cal 1/College Algebra"},
+            {2, "Am I Right?"},
+            {3, "Hello?"},
+            {4, "Come up?"},
+            {5, "Ack"},
+            {6, "Lets Go"}
+        };
+    }
+}
